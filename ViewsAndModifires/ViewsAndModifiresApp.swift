499c0a04dd5128a787ed514c3f5513ba51e0aa31
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-01 19:31:00 +0200

    Initial Commit

---
+++ ViewsAndModifires/ViewsAndModifiresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiresApp.swift
+//  ViewsAndModifires
+//
+//  Created by Mój Maczek on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
